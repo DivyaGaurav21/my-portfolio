0a72aed5df90fcd587bbb95aefc1ae0872bc7a08
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2022-12-28 19:36:09 +0530

    final setup of all component in application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './App.css';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Experience from './components/experience/Experience';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header'
+import Myservice from './components/myservice/Myservice';
+import Nav from './components/nav/Nav';
+import Portfolio from './components/portfolio/Portfolio';
+import Testimonials from './components/testimonials/Testimonials';
+
 
 function App() {
   return (
-    <div className="App">
-      <p>Divya Gaurav</p>
+      <div className="App">
+          <Header />
+          <Nav />
+          <About />
+          <Experience />
+          <Myservice />
+          <Portfolio />
+          <Testimonials />
+          <Contact />
+          <Footer />
     </div>
   );
 }
---
+++ src/components/myservice/Myservice.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Myservice.css'
+
+const Myservice = () => {
+  return (
+    <div>Myservice</div>
+  )
+}
+
+export default Myservice
