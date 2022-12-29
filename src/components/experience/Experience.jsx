3d9df34f535aff3da5c9aeb17f96092f3b4e4b38
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2022-12-29 16:49:24 +0530

    compleated Experince and service section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,116 @@
 import React from 'react'
 import './Experience.css'
+import { BsFillPatchCheckFill } from 'react-icons/bs'
+import { FaJava } from 'react-icons/fa'
+import { FaCode } from 'react-icons/fa'
+import { RiDatabase2Line } from 'react-icons/ri'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experiencee</section>
+      <section id='experience'>
+          <h5>What Skills I Have</h5>
+          <h2>My Experience</h2>
+
+     <div className="container experience__container">
+              <div className="expeience__frontend">
+                  <h3>Frontend Developement</h3>
+                  <div className="experience__content">
+                      <article className="experience__details">
+                          <BsFillPatchCheckFill className='exp_icon'/>
+                          <div>
+                              <h4>HTML</h4>
+                              <small className="text-light">Experienced</small>
+                          </div>
+                      </article>
+                      <article className="experience__details">
+                          <BsFillPatchCheckFill className='exp_icon' />
+                          <div>
+                              <h4>CSS</h4>
+                              <small className="text-light">Experienced</small>
+                          </div>
+                      </article>
+                      <article className="experience__details">
+                          <BsFillPatchCheckFill className='exp_icon' />
+                          <div>
+                              <h4>JavaScript</h4>
+                              <small className="text-light">Experienced</small>
+                          </div>
+                      </article>
+                      <article className="experience__details">
+                          <BsFillPatchCheckFill className='exp_icon' />
+                          <div>
+                              <h4>MaterialUI</h4>
+                              <small className="text-light">Intermediate</small>
+                          </div>
+                      </article>
+                      <article className="experience__details">
+                          <BsFillPatchCheckFill className='exp_icon' />
+                          <div>
+                              <h4>React</h4>
+                              <small className="text-light">Experienced</small>
+                          </div>
+                      </article>
+                      <article className="experience__details">
+                          <BsFillPatchCheckFill className='exp_icon' />
+                          <div>
+                              <h4>Redux</h4>
+                              <small className="text-light">Experienced</small>
+                          </div>
+                      </article>
+                  </div>
+              </div>
+              {/* END OF FRONTEND  */}
+              <div className="expeience__backend">
+                  <h3>Backend Developement</h3>
+                  <div className="experience__content">
+                      <article className="experience__details">
+                          <BsFillPatchCheckFill className='exp_icon' />
+                          <div>
+                              <h4>NodeJS</h4>
+                              <small className="text-light">Intermediate</small>
+                          </div>
+                      </article>
+                      <article className="experience__details">
+                          <BsFillPatchCheckFill className='exp_icon' />
+                          <div>
+                              <h4>Express</h4>
+                              <small className="text-light">Experienced</small>
+                          </div>
+                      </article>
+                      <article className="experience__details">
+                          <RiDatabase2Line className='exp_icon' />
+                          <div>
+                              <h4>MongoDB</h4>
+                              <small className="text-light">Intermediate</small>
+                          </div>
+                      </article>
+                      <article className="experience__details">
+                          <RiDatabase2Line className='exp_icon' />
+                          <div>
+                              <h4>MySQL</h4>
+                              <small className="text-light">Expeienced</small>
+                          </div>
+                      </article>
+                      <article className="experience__details">
+                          <FaCode className='exp_icon' />
+                          <div>
+                              <h4>DSA</h4>
+                              <small className="text-light">Intermediate</small>
+                          </div>
+                      </article>
+                      <article className="experience__details">
+                          <FaJava className='exp_icon' />
+                          <div>
+                              <h4>Java</h4>
+                              <small className="text-light">Experienced</small>
+                          </div>
+                      </article>
+                  </div>
+              </div>
+      
+     </div>
+         
+    </section>
   )
 }
 
--- src/components/myservice/Myservice.jsx
+++ src/components/myservice/Myservice.jsx
@@ -1,9 +1,116 @@
 import React from 'react'
 import './Myservice.css'
+import { BsCheck } from 'react-icons/bs'
 
 const Myservice = () => {
-  return (
-   <section id="myservice">Serviceee</section>
+    return (
+        <section id="myservice">
+            <h5>What I offer</h5>
+            <h2>Services</h2>
+
+            <div className="container service__container">
+                {/* UI/UX Design  */}
+                <article className="service">
+                    <div className="service__head">
+                        <h3>UI/UX Design</h3>
+                    </div>
+                    <ul className="service__list">
+                        <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>design ideas using storyboards, process and sitemaps</p>
+                        </li>
+                        <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>Design graphical UI, like menus, tabs and widgets</p>
+                        </li>
+                        <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>Build page navigation buttons and search fields</p>
+                        </li>
+                        <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>Develop UI mockups and prototypes</p>
+                        </li>
+                        <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>Create original graphic designs</p>
+                        </li>
+                        <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>Evaluate user requirements in collaboration</p>
+                        </li>
+                    </ul>
+                </article>
+                {/* WEB DEVELOPEMENT  */}
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Web Developement</h3>
+                    </div>
+                    <ul className="service__list">
+                        <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>Writing less Code In react and build user requirement UI</p>
+                        </li>
+                        <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>Wekk Knowledge of react and redux for building Complex UI</p>
+                        </li>
+                        <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>Create an API by Node(Express)</p>
+                        </li>
+                        <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>Testing web applications</p>
+                        </li>
+                        <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>Well Knowledge of MYSQL and MongoDB database</p>
+                        </li>
+                        <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>Having an Idea about How i Design a FullStack WEB</p>
+                        </li>
+                        <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>Troubleshooting problems with performance or user experience.</p>
+                        </li>
+                    </ul>
+                </article>
+                {/* CONTENT CREATION  */}
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Contact Creation</h3>
+                    </div>
+                    <ul className="service__list">
+                        <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>Blogs:high-quality, and well-written pieces about topics</p>
+                        </li>
+                        <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>CaseStudio:Case studies are a good way of making issues relatable</p>
+                        </li>
+                        <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>Listicles:List articles may seem somewhat gimmicky</p>
+                        </li>
+                        <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>Ebook:type of long-form content, and as the name suggests,</p>
+                        </li>
+                        <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>nfographics are among the most popular types of content</p>
+                        </li>
+                        {/* <li>
+                            <BsCheck className='sevice__list-icon'/>
+                            <p>Gather and evaluate user requirements in collaboration</p>
+                        </li> */}
+                    </ul>
+                </article>
+            </div>
+        </section>
   )
 }
 
