404c2794c6d7d773c4356700208a70b3d17109cd
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2022-12-29 22:13:07 +0530

    compleated testimonial section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,83 @@
 import React from 'react'
 import './Portfolio.css'
+import IMG1 from '../../assets/portfolio1.png'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.png'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+const data = [
+    {
+        id: 1,
+        image: IMG1,
+        title: 'IMDb Clone Website',
+        github: 'https://github.com/DivyaGaurav21/IMDb_Clone-website',
+        demo: 'https://divyagaurav21.github.io/IMDb_Clone-website/'
+    },
+    {
+        id: 2,
+        image: IMG4,
+        title: 'Smart Todo List',
+        github: 'https://github.com/DivyaGaurav21/TodoList_Vanilla',
+        demo: 'https://divyagaurav21.github.io/TodoList_Vanilla/'
+    },
+    {
+        id: 3,
+        image: IMG3,
+        title: 'Finance service app',
+        github: 'https://github.com/DivyaGaurav21',
+        demo: 'https://dribbble.com/shots/20247404-Finance-service-Mobile-app'
+    },
+    {
+        id: 4,
+        image: IMG2,
+        title: 'Smart Home LC',
+        github: 'https://github.com/DivyaGaurav21',
+        demo: 'https://dribbble.com/shots/20242498-iHome-Smart-Home-Light-Controls'
+    },
+    {
+        id: 5,
+        image: IMG5,
+        title: 'Energy Expenses Platform',
+        github: 'https://github.com/DivyaGaurav21',
+        demo: 'https://dribbble.com/shots/20244694-Energy-Expenses-Platform'
+    },
+    {
+        id: 6,
+        image: IMG6,
+        title: 'Landing New Year',
+        github: 'https://github.com/DivyaGaurav21',
+        demo: 'https://dribbble.com/shots/20242163-Happy-New-Year'
+    }
+]
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfoliooo</section>
+      <section id="portfolio">
+          <h5>My Reacent Work</h5>
+          <h2>Portfolio</h2>
+
+          <div className="container portfolio__container">
+              
+          {
+                  data.map(item => (
+                          <article key={item.id} className="portfolio__item">
+                              <div className="portfolio__item-image">
+                                  <img src={item.image} alt="imageport" />
+                              </div>
+                          <h3>{ item.title}</h3>
+                              <div className="portfolio__item-cta">
+                                  <a href={item.github} target='_blank' rel='noreferrer' className='btn'>Github</a>
+                                  <a href={item.demo} target='_blank' rel='noreferrer' className='btn btn-primary'>Live Demo</a>
+                              </div>
+                          </article>
+                )
+            )      
+         }
+              
+          </div>
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,73 @@
 import React from 'react'
 import './Testimonials.css'
+import AVTR1 from '../../assets/avatar2.jpg'
+import AVTR2 from '../../assets/avatar3.jpg'
+import AVTR3 from '../../assets/avatar4.jpg'
+import AVTR4 from '../../assets/avatar1.jpg'
+
+// import Swiper core and required modules
+import {Pagination} from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+    {
+        avatar: AVTR1,
+        name: 'Johny Baristow',
+        review: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ut ipsum et accusantium officia exercitationem sed cumque quo maiores, omnis, maxime laborum voluptates dolorem non aut quibusdam? Veniam, cum sit!'
+    },
+    {
+        avatar: AVTR2,
+        name: 'Jofra Archer',
+        review: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ut ipsum et accusantium officia exercitationem sed cumque quo maiores, omnis, maxime laborum voluptates dolorem non aut quibusdam? Veniam, cum sit!'
+    },
+    {
+        avatar: AVTR3,
+        name: 'Melvis Timtim',
+        review: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ut ipsum et accusantium officia exercitationem sed cumque quo maiores, omnis, maxime laborum voluptates dolorem non aut quibusdam? Veniam, cum sit!'
+    },
+    {
+        avatar: AVTR4,
+        name: 'Sania Mirja',
+        review: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ut ipsum et accusantium officia exercitationem sed cumque quo maiores, omnis, maxime laborum voluptates dolorem non aut quibusdam? Veniam, cum sit!'
+    }
+]
 
 const Testimonials = () => {
   return (
-    <section id="testimonials">Testimonials</section>
+      <section id="testimonials">
+          <h5>Review from client</h5>
+          <h2>Testimonials</h2>
+
+          <Swiper className="container testimonials__container"
+              // install Swiper modules
+              modules={[Pagination]}
+              spaceBetween={40}
+              slidesPerView={1}
+              navigation
+              pagination={{ clickable: true }}
+              scrollbar={{ draggable: true }}
+              onSwiper={(swiper) => console.log(swiper)}
+              onSlideChange={() => console.log('slide change')}
+          >
+              {
+                  data.map(item => (
+                      <SwiperSlide key={item.name} className='testimonial'>
+                          <div className="client__avatar">
+                              <img src={item.avatar} alt="avatar-imge" />
+                          </div>
+                          <h5 className='client__name'>{ item.name}</h5>
+                          <small className='client__review'>{ item.review}</small>
+                      </SwiperSlide>
+                  ))
+              }
+             
+          </Swiper>
+    </section>
   )
 }
 
